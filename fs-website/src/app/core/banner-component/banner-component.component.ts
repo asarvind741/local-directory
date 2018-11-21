9ab05f5527d115f244be257421bce6000cdb97d7
Arvind Singh <arvindsingh741234@gmail.com>
2018-11-21 22:09:34 +0530

    test

--- fs-website/src/app/core/banner-component/banner-component.component.ts
+++ fs-website/src/app/core/banner-component/banner-component.component.ts
@@ -10,71 +10,11 @@ declare let Linear: any
   templateUrl: './banner-component.component.html',
   styleUrls: ['./banner-component.component.css']
 })
-export class BannerComponentComponent implements OnInit, AfterViewInit {
+export class BannerComponentComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-
-    function spinGlobe() {
-      var tmax_tl = new TimelineMax({
-            delay: 0.1675,
-            repeat: -1 
-          });
-    
-      var globe_continents = [
-            $('#globe #middle g path'),
-            $('#globe #left g path')
-          ];
-    
-      var globe_speed = 10;
-    
-      var map_from = {
-        x: 0
-      };
-    
-      var map_to = {
-        x: 150,
-        ease: easeLinear
-      };
-    
-      tmax_tl.fromTo(globe_continents, globe_speed, map_from, map_to, 0);
-      
-      return tmax_tl;
-    }
-    
-    spinGlobe();
-  }
-
-  ngAfterViewInit(){
-    function spinGlobe() {
-      var tmax_tl = new TimelineMax({
-            delay: 0.1675,
-            repeat: -1 
-          });
-    
-      var globe_continents = [
-            $('#globe #middle g path'),
-            $('#globe #left g path')
-          ];
-    
-      var globe_speed = 10;
-    
-      var map_from = {
-        x: 0
-      };
-    
-      var map_to = {
-        x: 150,
-        ease: Linear.easeOut
-      };
-    
-      tmax_tl.fromTo(globe_continents, globe_speed, map_from, map_to, 0);
-      
-      return tmax_tl;
-    }
-    
-    spinGlobe();
   }
 
 }

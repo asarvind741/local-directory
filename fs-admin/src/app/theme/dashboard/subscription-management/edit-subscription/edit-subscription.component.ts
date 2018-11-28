9509d70b114c8cc6cf71f8102c724f98170e541e
Arvind Singh <c-bhattm@kapsarc.org>
2018-11-28 19:34:43 +0530

    test

--- fs-admin/src/app/theme/dashboard/subscription-management/edit-subscription/edit-subscription.component.ts
+++ fs-admin/src/app/theme/dashboard/subscription-management/edit-subscription/edit-subscription.component.ts
@@ -14,7 +14,22 @@ export class EditSubscriptionComponent implements OnInit {
   editPlanForm: FormGroup;
   showMessage: any;
   statuss: Array<String> = ['Active', 'Inactive'];
-  duration: Array<String> = ['Yearly', 'Half Yearly', 'Quarterly', 'Monthly']
+  duration: Array<String> = ['Yearly', 'Half Yearly', 'Quarterly', 'Monthly'];
+  role: Array<Object> = [
+    {'id': 1, 'itemName': 'Buyer' }, 
+    {'id': 2, 'itemName': 'Supplier' }, 
+    {'id': 3, 'itemName': 'Agent' }, 
+    {'id': 4, 'itemName': 'Reseller' }];
+    modulesToInclude: Array<Object> = [
+      {'id': 1, 'itemName': 'First Module' }, 
+      {'id': 2, 'itemName': 'Second Module' }, 
+      {'id': 3, 'itemName': 'Third Module' }, 
+      {'id': 4, 'itemName': 'Fourth Module' }];
+      selectedModules: any;
+      selectedRoles: any;
+      settings1: any;
+      settings: any;
+
   @Input() currentPlan;
   constructor(
     public activeModal: NgbActiveModal,
@@ -22,6 +37,23 @@ export class EditSubscriptionComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.settings = {
+      singleSelection: false,
+      text: "Select Roles",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      badgeShowLimit: 3
+    };
+
+    this.settings1 = {
+      singleSelection: false,
+      text: "Select Modules",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      badgeShowLimit: 3
+    };
     this.createForm();
   }
 
@@ -31,12 +63,21 @@ export class EditSubscriptionComponent implements OnInit {
     let price = this.currentPlan.price ? this.currentPlan.price : '';
     let description = this.currentPlan.description ? this.currentPlan.description: '';
     let status = this.currentPlan.status ? this.currentPlan.status : null;
+    let maxNumberOfMembers = this.currentPlan.maxNumberOfMembers ? this.currentPlan.maxNumberOfMembers : null;
+    let rolesAllowed = this.currentPlan.rolesAllowed ? this.currentPlan.rolesAllowed : null;
+    let moduleIncluded = this.currentPlan.moduleIncluded ? this.currentPlan.moduleIncluded : null;
+    this.selectedRoles = rolesAllowed;
+    this.selectedModules = moduleIncluded;
+    
     this.editPlanForm = new FormGroup({
       'name': new FormControl(name),
       'duration': new FormControl(duration),
       'price': new FormControl(price),
       'description': new FormControl(description),
-      'status': new FormControl(status)
+      'status': new FormControl(status),
+      'maxNumberOfMembers': new FormControl(maxNumberOfMembers),
+      'rolesAllowed': new FormControl(rolesAllowed),
+      'moduleIncluded': new FormControl(moduleIncluded)
     })
   }
 
@@ -82,6 +123,15 @@ export class EditSubscriptionComponent implements OnInit {
 
   clearModal(){
     this.editPlanForm.reset();
+  }
+
+  onItemSelect(item: any) {
+  }
+  OnItemDeSelect(item: any) {
+  }
+  onSelectAll(items: any) {
+  }
+  onDeSelectAll(items: any) {
   }
 
 }
